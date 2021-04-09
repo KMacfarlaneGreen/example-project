13890206226db82e8c0d56734d42174f7fa6b6ef
joycelynlongdon <joycelynlongdon@gmail.com>
2021-04-09 15:59:05 +0100

    non_lin_reg

--- my_ml_project/linear_regression_joycelyn.py
+++ my_ml_project/linear_regression_joycelyn.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def lin_reg(x,y):
-    '''Function that implements linear regression'''
-    ones = np.ones_like(x) # create a vector of 1's with the same length as x
 
-    X = np.stack([ones, x], axis = 1) # stack 1's and x's to get the X matrix having the 1's and x's as columns (design matrix for linear regression)
+def lin_reg(x, y):
+    """Function that implements linear regression"""
+    ones = np.ones_like(x)  # create a vector of 1's with the same length as x
 
-    w = np.linalg.solve((X.T).dot(X) , (X.T).dot(y)) # compute the optimal w using the Moore-Penrose pseudoinverse
+    X = np.stack(
+        [ones, x], axis=1
+    )  # stack 1's and x's to get the X matrix having the 1's and x's as columns (design matrix for linear regression)
+
+    w = np.linalg.solve(
+        (X.T).dot(X), (X.T).dot(y)
+    )  # compute the optimal w using the Moore-Penrose pseudoinverse
 
     # The above line is equivalent to the following:
     # w = np.linalg.inv((X.T).dot(X)).dot(X.T).dot(y_lin)
@@ -15,12 +20,15 @@ def lin_reg(x,y):
     # instead it uses a linear solver, which is more stable. In general, if you don't explicitly need a matrix inverse,
     # you should avoid computing it.
 
-    x_pred = np.linspace(0, 1, 100) # the range depends on the range of data but for an example set 100 points equispaced between 0 and 1
+    x_pred = np.linspace(
+        0, 1, 100
+    )  # the range depends on the range of data but for an example set 100 points equispaced between 0 and 1
 
-    y_pred = (w[1] * x_pred) + w[0] # evaluate the linear trendline at the values of x above
+    y_pred = (w[1] * x_pred) + w[
+        0
+    ]  # evaluate the linear trendline at the values of x above
 
-    plt.plot(x_pred, y_pred, color = 'black') # plot the trendline
-    plt.scatter(x, y, marker = 'x', color = 'red') # plot the datapoints
-    beautify_plot({"title":"Least squares regression", "x":"$x$", "y":"$y$"})
+    plt.plot(x_pred, y_pred, color="black")  # plot the trendline
+    plt.scatter(x, y, marker="x", color="red")  # plot the datapoints
     plt.show()
-    print(w[1],w[0])
+    print(w[1], w[0])
--- my_ml_project/non_linear_regression.py
+++ my_ml_project/non_linear_regression.py
@@ -1,31 +1,35 @@
-D = 5  # D represents the order of the polynomial
-
-phi = np.array(
-    [[x_ ** d for d in range(D + 1)] for x_ in x_nonlin]
-)  # build the design matrix Phi
-
-# compute the optimal w using the Moore-Penrose pseudoinverse
-w = np.linalg.solve((phi.T).dot(phi), (phi.T).dot(y_nonlin))
-
-# As with linear regression, the line above is numerically stable version of line commented line below (e.g. it works with D>N)
-# w = np.linalg.inv((phi.T).dot(phi)).dot(phi.T).dot(y_nonlin) # apply the Moore-Penrose pseudoinverse using Phi
-
-x_pred = np.linspace(0, 1, 100)  # 100 points equispaced between 0 and 1
-
-phi_pred = np.array(
-    [[x_ ** d for d in range(D + 1)] for x_ in xs]
-)  # design matrix for points at which to plot
-
-y_pred = phi_pred.dot(w)  # output of the model at the points above
+import numpy
+import matplotlib as plt
 
 
-######## Plotting code #######
-print(
-    "Sum squared errors for polynomial of order {}:".format(D),
-    np.sum((phi.dot(w) - y_nonlin) ** 2).round(3),
-)
-plt.scatter(x_nonlin, y_nonlin, marker="x", color="red")  # plot model predictions
-plt.plot(xs, ys, color="black")  # plot dataset
-beautify_plot({"title": "Polynomial fit (D = {})".format(D), "x": "x", "y": "y"})
+def non_lin_reg(x_nonlin, y_nonlin):
 
-plt.show()
+    D = 5  # D represents the order of the polynomial
+
+    phi = np.array(
+        [[x_ ** d for d in range(D + 1)] for x_ in x_nonlin]
+    )  # build the design matrix Phi
+
+    # compute the optimal w using the Moore-Penrose pseudoinverse
+    w = np.linalg.solve((phi.T).dot(phi), (phi.T).dot(y_nonlin))
+
+    # As with linear regression, the line above is numerically stable version of line commented line below (e.g. it works with D>N)
+    # w = np.linalg.inv((phi.T).dot(phi)).dot(phi.T).dot(y_nonlin) # apply the Moore-Penrose pseudoinverse using Phi
+
+    x_pred = np.linspace(0, 1, 100)  # 100 points equispaced between 0 and 1
+
+    phi_pred = np.array(
+        [[x_ ** d for d in range(D + 1)] for x_ in xs]
+    )  # design matrix for points at which to plot
+
+    y_pred = phi_pred.dot(w)  # output of the model at the points above
+
+    ######## Plotting code #######
+    print(
+        "Sum squared errors for polynomial of order {}:".format(D),
+        np.sum((phi.dot(w) - y_nonlin) ** 2).round(3),
+    )
+    plt.scatter(x_nonlin, y_nonlin, marker="x", color="red")  # plot model predictions
+    plt.plot(x_pred, y_pred, color="black")  # plot dataset
+
+    plt.show()
