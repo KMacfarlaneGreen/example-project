e5a1c48be6fc1b495ed9414612711e76161f727e
franciscovargas <vargfran@gmail.com>
2021-04-28 14:57:13 +0100

    Further refatorings

--- my_ml_project/linear_regression_joycelyn.py
+++ my_ml_project/linear_regression_joycelyn.py
@@ -2,14 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def lin_reg(x, y):
+# 1. fit w
+# 2 predict w
+# 3. mse 
+
+#  Homework ? get this to work ?
+class LinearRegression(object):
+    # This code might not run (try fixing)    
+    def __init__(self, dim=1, _lambda =0.001):
+        # class attributes can be accesed by any method (function) whose first argument is self
+        self.w = np.zeros(dim) # 
+#         self.lambda = _lambda
+        self.dim = dim
+    
+    def fit(self, x, y, bias=True):
+        """Function that implements linear regression"""
+        ones = np.ones_like(x)  # create a vector of 1's with the same length as x
+
+        if bias:
+            X = np.stack(
+                [ones, x], axis=1
+            )  # stack 1's and x's to get the X matrix having the 1's and x's as columns (design matrix for linear regression)
+        else:
+            X = np.array(x)
+        # np.linalg.cho_solve
+        self.w = np.linalg.solve(
+            (X.T).dot(X) + self.lambbda * np.eye(self.dim), (X.T).dot(y)
+        )  # compute the optimal w using the Moore-Penrose pseudoinverse
+    
+    
+    def predict(self, x_test, bias=True):
+        ones = np.ones_like(x_test) 
+        if bias:
+            X = np.stack(
+                [ones, x_test], axis=1
+            ) 
+
+        y_pred = X.dot(self.w.T) 
+        return y_pred
+
+    
+# Classes vs functions
+    
+# # class way (sklearn) 
+# model = LinearRegression(dim=1, _lambda=0.001)
+# model.fit(x, y, lambda_) # I dont have to pas it lambda
+# model.predict(x) # I dont have pass it w 
+
+# #  Function way
+# lambda_ = 0.001
+# w = lin_reg(x,y, lambda_)
+# y_pred = predict_linear(x_test, w)
+# w = lin(x,y, lmabga_)
+# y_pred_2 = predict_linear(x_test_2, w)
+    
+        
+          
+
+def lin_reg(x, y, bias=True):
     """Function that implements linear regression"""
     ones = np.ones_like(x)  # create a vector of 1's with the same length as x
-
-    X = np.stack(
-        [ones, x], axis=1
-    )  # stack 1's and x's to get the X matrix having the 1's and x's as columns (design matrix for linear regression)
-
+    
+    if bias:
+        X = np.stack(
+            [ones, x], axis=1
+        )  # stack 1's and x's to get the X matrix having the 1's and x's as columns (design matrix for linear regression)
+    else:
+        X = np.array(x)
+        
+    # np.linalg.cho_solve
+    import pdb; pdb.set_trace()
     w = np.linalg.solve(
         (X.T).dot(X), (X.T).dot(y)
     )  # compute the optimal w using the Moore-Penrose pseudoinverse
@@ -20,15 +82,27 @@ def lin_reg(x, y):
     # instead it uses a linear solver, which is more stable. In general, if you don't explicitly need a matrix inverse,
     # you should avoid computing it.
 
+    return w
+
+
+def predict_linear(x_test, w, bias=True):
+    
+    # X_test N x D , w 1 x D    X * w.T ( NxD * Dx1) = N x 1 )
+    # Phi N x K K >>> D, w = 1xK
+    ones = np.ones_like(x_test) 
+    if bias:
+        X = np.stack(
+            [ones, x_test], axis=1
+        ) 
+    
+    y_pred = X.dot(w.T)  # ( (w * X.T).T = X * w.T 
+    return y_pred
+
+    
+    
+def test_lin_regressor():
     x_pred = np.linspace(
         0, 1, 100
     )  # the range depends on the range of data but for an example set 100 points equispaced between 0 and 1
 
-    y_pred = (w[1] * x_pred) + w[
-        0
-    ]  # evaluate the linear trendline at the values of x above
-
-    plt.plot(x_pred, y_pred, color="black")  # plot the trendline
-    plt.scatter(x, y, marker="x", color="red")  # plot the datapoints
-    plt.show()
-    print(w[1], w[0])
+    y_pred = predict(x_pred) # evaluate the linear trendline at the values of x above
--- my_ml_project/non_linear_regression.py
+++ my_ml_project/non_linear_regression.py
@@ -1,35 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from my_ml_project.linear_regression_joycelyn import lin_reg, predict_linear 
+
+
+
+def featurize_phi(x, D=5):
+    phi = np.array(
+        [[x_ ** d for d in range(D + 1)] for x_ in x]
+    )
+    
+    return phi
 
 
 def non_lin_reg(x_nonlin, y_nonlin):
 
-    D = 5  # D represents the order of the polynomial
+    phi = featurize_phi(x_nonlin)
+    
+    # abstraction 
+    w = lin_reg(phi, y_nonlin, bias=False)
+    
+    return w
 
-    phi = np.array(
-        [[x_ ** d for d in range(D + 1)] for x_ in x_nonlin]
-    )  # build the design matrix Phi
 
-    # compute the optimal w using the Moore-Penrose pseudoinverse
-    w = np.linalg.solve((phi.T).dot(phi), (phi.T).dot(y_nonlin))
-
-    # As with linear regression, the line above is numerically stable version of line commented line below (e.g. it works with D>N)
-    # w = np.linalg.inv((phi.T).dot(phi)).dot(phi.T).dot(y_nonlin) # apply the Moore-Penrose pseudoinverse using Phi
-
-    x_pred = np.linspace(0, 1, 100)  # 100 points equispaced between 0 and 1
-
-    phi_pred = np.array(
-        [[x_ ** d for d in range(D + 1)] for x_ in x_pred]
-    )  # design matrix for points at which to plot
-
-    y_pred = phi_pred.dot(w)  # output of the model at the points above
-
-    ######## Plotting code #######
-    print(
-        "Sum squared errors for polynomial of order {}:".format(D),
-        np.sum((phi.dot(w) - y_nonlin) ** 2).round(3),
-    )
-    plt.scatter(x_nonlin, y_nonlin, marker="x", color="red")  # plot model predictions
-    plt.plot(x_pred, y_pred, color="black")  # plot dataset
-
-    plt.show()
+def predict(x_test, w):
+    
+    phi = featurize_phi(x_test)
+    
+    return predict_linear(phi, w, bias=False)
+#     return phi.dot(w.T)
+    
